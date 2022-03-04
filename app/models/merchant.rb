2428764f511856f5ba8fc06555c3e69db54a3864
Conor Barthel <conor.barthel@gmail.com>
2022-03-03 18:11:32 -0700

    Feat:Relationship and test for bulk discount

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,7 @@ class Merchant < ApplicationRecord
   enum status: {enabled: 0, disabled: 1}
   validates :name, presence: true
 
+  has_many :bulk_discounts
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
