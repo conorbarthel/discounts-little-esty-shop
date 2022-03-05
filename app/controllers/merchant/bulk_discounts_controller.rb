ab5b2b14e38b8e689fb9f98bcd2b41349bd219e8
Conor Barthel <conor.barthel@gmail.com>
2022-03-05 11:36:37 -0700

    Feat: Create a new bulk_discount

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -7,6 +7,24 @@ class Merchant::BulkDiscountsController < ApplicationController
   end
 
   def new
-    @bulk_discount = BulkDiscount.new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new
   end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = merchant.bulk_discounts.create(bulk_discount_params)
+    if bulk_discount.id == nil
+      flash[:alert] = "All fields must be filled in with valid data"
+      redirect_to new_merchant_bulk_discount_path(merchant)
+    else
+      redirect_to merchant_bulk_discounts_path(merchant)
+    end
+  end
+
+  private
+    def bulk_discount_params
+      params.require(:bulk_discount).permit(:percentage_discount,
+                                            :quantity_threshold)
+    end
 end
--- spec/features/merchant/bulk_discounts/new_spec.rb
+++ spec/features/merchant/bulk_discounts/new_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'The Bulk Discounts New Page' do
   end
 
   it "is linked to from the merchant bulk discounts page" do
-    visit merchant_dashboard_index_path(@katz)
+    visit merchant_bulk_discounts_path(@katz)
     click_on "Create New Discount"
 
     expect(current_path).to eq(new_merchant_bulk_discount_path(@katz))
@@ -15,11 +15,12 @@ RSpec.describe 'The Bulk Discounts New Page' do
   it "When the form is submitted with valid data user is redirected to
   bulk discount index page" do
     visit new_merchant_bulk_discount_path(@katz)
-    fill_in 'percentage_discount', with: 35
-    fill_in 'quantity_threshold', with: 40
-    click_on 'Create Bulk Discount'
 
-    expect(current_path).to eq(merchant_dashboard_index_path(@katz))
+    fill_in 'Percentage discount', with: 35
+    fill_in 'Quantity threshold', with: 40
+    click_on 'Create Bulk discount'
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@katz))
   end
 
 
